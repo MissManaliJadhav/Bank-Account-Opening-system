55c94639d6f677db8c8a83f51c5656354697c74e
Manali Mahendra Jadhav <mmjadhav@dbatu.ac.in>
2026-03-26 15:54:22 +0530

    Update risk_engine.py

--- python-service/risk_engine.py
+++ python-service/risk_engine.py
@@ -1,23 +1,15 @@
 def calculate_risk(data):
     score = 0
 
-    amount = data.get("amount", 0)
-    source = data.get("source", "").lower()
-    international = data.get("international", False)
-
-    # RULE 1
-    if source == "cash" and amount > 50000:
+    if data.get("source") == "cash" and data.get("amount", 0) > 50000:
         score += 40
 
-    # RULE 2
-    if source == "crypto":
+    if data.get("source") == "crypto":
         score += 50
 
-    # RULE 3
-    if international:
+    if data.get("international"):
         score += 30
 
-    # LEVEL
     if score < 30:
         level = "LOW"
     elif score <= 70:
